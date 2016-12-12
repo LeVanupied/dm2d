2e0c0a649ee0620fe61bf19ca34d4c83fe66b016
wburgos <wburgos@student.42.fr>
2016-12-12 01:27:54 +0100

    modifiation du pot, tentative de verlet

--- testcode.f90
+++ testcode.f90
@@ -1,9 +1,9 @@
 module general
 implicit none
-integer :: i, p
+integer :: i, p, j
 integer, parameter :: a = 10, npart = 100, T = 500
 real(8), dimension(npart, 2) :: pos
-real(8), parameter :: R = 0.1, m = 0.00001, v0 = 1, U0 = 1
+real(8), parameter :: R = 0.1, dt=0.1, m = 0.00001, v0 = 1, U0 = 1
 
 
 end module general
@@ -11,10 +11,52 @@ end module general
 program deuxD
 use general
 implicit none
+real(8) :: v1, v2, r0, r1, r2, a0, a1, a2, xi, yi, dt = 0.01
+!Verlet
+call PosPart      !crée positions initiale
+call LennardJones !potentiel interatomes
 
+real(8) :: gauss, v1, v2, r0, r1, r2, a0, a1, a2, xi, yi, dt = 0.01
+open(20, file = 'newpos.res')
+read(10, *) x, y
+do t = 1, T; do i = 1, npart
+  dt = t*dt
+!call newpos.res if it doesn't exist call use PosPart
+
+  do i = 1, npart - 1; do p = i + 1, npart                 !pot ressenti par la part i des p autres part
+    dx(i, p) = pos(i, 1) - pos(p, 1)                    !distance en x de la ième part avec les p autrees part
+    dy(i, p) = pos(i, 2) - pos(p, 2)
+  end do
+
+  r = sqrt(minval(dx)**2 + minval(dy)**2)       !!distance en y de la ième part avec les p autrees part
+  a0 = f(1) / m
+  r1 = r
+  v0 = gauss()
+  v1 = v0
+    do j = 1, npart
+
+      r2 = r1 + dt * v1 +((dt ** 2) / 2) * a0               !1=t et 2=t+1
+      a1 = 24 / m * (2 * (R ** 12 / r2 ** 13) - ( R ** 6 / r2 ** 7))
+      v2 = v1 + (dt / 2 ) * (a0 + a1)
+    enddo
+
+  r1 = r2                                    !nouvelles valeurs
+  v1 = v2
+  a0 = a1
+enddo
+
+write(20, *) r1, v1, a0
+enddo
+close(20)
+end
+
+close(20)
 
 call PosPart
 end program deuxD
+
+
+
 
 !Position aleatoire des particules
 subroutine PosPart
@@ -24,7 +66,7 @@ real(8) xi, yi, xp, yp
 
 open(10, file = 'positions.res')
 
-createpoint: do i = 1, npart            !tirage de position alÃ©atoire
+createpoint: do i = 1, npart            !tirage de position aleatoire
 call random_number(xi)               !0<x<1, jaimerais faire un vrai nombre aléatoire entre 0 et a
 call random_number(yi)               !0<y<1
 pos(i, 1) = xi
@@ -45,39 +87,8 @@ close(10)
 
 end
 
-subroutine Verlet
-use general
-implicit none
-real(8) :: v1, v2, r0, r1, r2, a0, a1, a2, xi, yi, dt = 0.01
+!Verlet
 
-open(20, file = 'verlet.res')
-
-
-do i = 1, npart, 1
-read(10, *) xi, yi
-
-r0 = sqrt(xi ** 2 + yi ** 2)
-
-a0 = 24 / m * (2 * (R ** 12 / r0 ** 13) - (R ** 6 / r0 ** 7))
-r1 = r0
-v1 = v0
-
-    do p = 0, T, 1
-
-    r2 = r1 + dt * v1 +((dt ** 2) / 2) * a0               !1=t et 2=t+1
-    a1 = 24 / m * (2 * (R ** 12 / r2 ** 13) - ( R ** 6 / r2 ** 7))
-    v2 = v1 + (dt / 2 ) * (a0 + a1)
-
-    r1 = r2                                    !nouvelles valeurs
-    v1 = v2
-    a0 = a1
-
-    enddo
-
-write(20, *) r1, v1, a0
-enddo
-close(20)
-end
 
 ! subroutine Pot_LennardJones
 ! use general
@@ -91,20 +102,48 @@ end
 ! end do
 ! end
 
-subroutine LennardJones
+subroutine LennardJones(r,U,f)
 use general
 implicit none
-real(8), dimension(npart - 1, npart - 1) :: U, dx, dy
-real(8), dimension(1:npart - 1) :: Ueff
-call PosPart
+real(8), intent(in) :: r
+real(8), intent(out) :: U, f
+real(8), dimension(npart - 1, npart - 1) :: dx, dy
+real(8), dimension(1:npart - 1) :: Upar, d
 
+f = 24 * (2 * (R ** 12 / r ** 13) - (R ** 6 / r ** 7))
 
 do i = 1, npart - 1; do p = i + 1, npart                 !pot ressenti par la part i des p autres part
   dx(i, p) = pos(i, 1) - pos(p, 1)                    !distance en x de la ième part avec les p autrees part
-  dy(i, p) = pos(i, 2) - pos(p, 2)                    !!distance en y de la ième part avec les p autrees part
-  U(i, p) = 4 * U0 * ((R / sqrt(dx(i, p) ** 2 + dy(i, p) ** 2)) ** 12 - (R / sqrt(dx(i, p) ** 2 + dy(i, p) ** 2)) ** 6)
-end do
-Ueff(i) = sum(U(i,:))      !somme de tous les elements de la ligne i
+  dy(i, p) = pos(i, 2) - pos(p, 2)
+  d(i) = sqrt(minval(dx)**2 + minval(dy)**2)                   !!distance en y de la ième part avec les p autrees part
 end do
 
+Upar(i) = 4 * U0 * ((R / d(i) ** 12 - (R / d(i)) ** 6)
+Ueff = sum(U(i))
+U = 4 * U0 * ((R / r ) ** 12 - (R / r) ** 6)
+
 end
+
+real function gauss()
+!    *******************************************************************
+!    ** RANDOM VARIATE FROM THE STANDARD NORMAL DISTRIBUTION.         **
+!    **                                                               **
+!    ** THE DISTRIBUTION IS GAUSSIAN WITH ZERO MEAN AND UNIT VARIANCE.**
+!    **                                                               **
+!    ** REFERENCE:                                                    **
+!    **                                                               **
+!    ** KNUTH D, THE ART OF COMPUTER PROGRAMMING, (2ND EDITION        **
+!    **    ADDISON-WESLEY), 1978                                      **
+!    **                                                               **
+!    *******************************************************************
+implicit none
+real, parameter :: a1 = 3.949846138, a3 = 0.252408784, a5 = 0.076542912, &
+                   a7 = 0.008355968, a9 = 0.029899776
+real :: r, r2, dummy
+real, dimension(12) :: s
+integer :: i
+
+call random_number(s) ; r = (sum(s)-6.0)/4.0 ; r2 = r*r
+gauss =  (((( a9 * r2 + a7 ) * r2 + a5 ) * r2 + a3 ) * r2 + a1 )*r
+end function gauss
+
